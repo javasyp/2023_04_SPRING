8383c5050fc9b4d58530973b0a1ceae5bc1f1294
SHINYOUNG <javasyp@gmail.com>
2023-04-13 17:22:56 +0900

    ArticleController 추가, 게시글 작성 구현, 리스트 구현

--- src/main/java/com/KoreaIT/syp/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/syp/demo/controller/UsrHomeController.java
@@ -9,11 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 @Controller
 public class UsrHomeController {
 	
@@ -73,49 +68,4 @@ public class UsrHomeController {
 		return list;	// []
 	}
 	
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목1");
-		
-		return article;
-	}
-	
-	@RequestMapping("/usr/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1");
-		Article article2 = new Article(2, "제목2");
-
-		List<Article> articles = new ArrayList<>();
-		articles.add(article1);
-		articles.add(article2);
-
-		return articles;
-	}
-}
-
-// 클래스 내부 private 변수를 전체 사용하려면 @Data 메소드를 써준다.
-@Data
-// 생성자를 만들지 않아도 된다.
-@AllArgsConstructor		// Article(int, String)
-@NoArgsConstructor		// Article()
-class Article {
-	// 외부에서 private 변수를 쓰려면 @Getter 메소드를 사용해야 한다.
-//	@Getter
-	private int id;
-//	@Getter
-	private String title;
-	
-	// @Getter 메소드가 없으면 이렇게 써줘야 한다.
-//	public int getId() {
-//		return id;
-//	}
-	
-	// @AllArgsConstructor 메소드 사용 시 필요없음
-//	public Article() {
-//		// this 생략
-//		id = 1;
-//		title = "제목";
-//	}
 }
