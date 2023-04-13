eb97cecf2b0c2991de15b25e125d15056e385b0d
SHINYOUNG <javasyp@gmail.com>
2023-04-13 16:52:25 +0900

    액션 메서드에서 각각의 리턴 타입 실험

--- src/main/java/com/KoreaIT/syp/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/syp/demo/controller/UsrHomeController.java
@@ -1,28 +1,121 @@
 package com.KoreaIT.syp.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UsrHomeController {
 	
-	private int count = 0;
-	
-	public UsrHomeController() {
-		count = 0;
-	}
-	
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getInt")
 	@ResponseBody
-	public int getCount() {
-		return count++;
+	public int getInt() {
+		return 10;
 	}
 	
-	@RequestMapping("/usr/home/setCount")
+	@RequestMapping("/usr/home/getString")
 	@ResponseBody
-	public String setCount(int count) {
-		this.count = count;
-		return "count의 값이 " + this.count + "으로 초기화됩니다.";
+	public String getString() {
+		return "안녕";
 	}
+	
+	@RequestMapping("/usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 10.5f;
+	}
+	
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
+	}
+	
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+	}
+	
+	@RequestMapping("/usr/home/getChar")
+	@ResponseBody
+	public char getChar() {
+		return 'a';
+	}
+	
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수 나이", 22);
+		map.put("영희 나이", 20);
+		
+		return map;		// {}
+	}
+	
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<Object> getList() {
+		List<Object> list = new ArrayList<>();
+		list.add("철수 나이");
+		list.add(22);
+		
+		return list;	// []
+	}
+	
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article(1, "제목1");
+		
+		return article;
+	}
+	
+	@RequestMapping("/usr/home/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		Article article1 = new Article(1, "제목1");
+		Article article2 = new Article(2, "제목2");
+
+		List<Article> articles = new ArrayList<>();
+		articles.add(article1);
+		articles.add(article2);
+
+		return articles;
+	}
+}
+
+// 클래스 내부 private 변수를 전체 사용하려면 @Data 메소드를 써준다.
+@Data
+// 생성자를 만들지 않아도 된다.
+@AllArgsConstructor		// Article(int, String)
+@NoArgsConstructor		// Article()
+class Article {
+	// 외부에서 private 변수를 쓰려면 @Getter 메소드를 사용해야 한다.
+//	@Getter
+	private int id;
+//	@Getter
+	private String title;
+	
+	// @Getter 메소드가 없으면 이렇게 써줘야 한다.
+//	public int getId() {
+//		return id;
+//	}
+	
+	// @AllArgsConstructor 메소드 사용 시 필요없음
+//	public Article() {
+//		// this 생략
+//		id = 1;
+//		title = "제목";
+//	}
 }
