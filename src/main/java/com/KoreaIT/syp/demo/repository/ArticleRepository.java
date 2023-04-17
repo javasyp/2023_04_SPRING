647fea4ace51d73abcb00198c7ed78166788acbc
SHINYOUNG <javasyp@gmail.com>
2023-04-17 13:05:56 +0900

    myBatis, JDBC 드라이버 추가, yml 파일에 접속 정보 추가, myBatis 활용해서 SELECT, INSERT, UPDATE, DELETE 구현

--- src/main/java/com/KoreaIT/syp/demo/repository/ArticleRepository.java
+++ src/main/java/com/KoreaIT/syp/demo/repository/ArticleRepository.java
@@ -1,70 +1,31 @@
 package com.KoreaIT.syp.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.KoreaIT.syp.demo.vo.Article;
 
-@Component
-public class ArticleRepository {
-	
-	private int lastArticleId;
-	private List<Article> articles;
-	
-	// 생성자
-	public ArticleRepository() {
-		lastArticleId = 0;
-		articles = new ArrayList<>();
-	}
-	
-	// 테스트 데이터
-	public void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목 " + i;
-			String body = "내용 " + i;
-			
-			writeArticle(title, body);
-		}
-	}
-	
-	// 작성
-	public Article writeArticle(String title, String body) {
-		int id = lastArticleId + 1;
+@Mapper
+public interface ArticleRepository {
 
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		lastArticleId++;
-		
-		return article;
-	}
-	
-	// 객체 데이터 가져오기
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {	// getter 메소드
-				return article;
-			}
-		}
-		return null;
-	}
-	
-	// 삭제
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-	}
-	
-	// 수정
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
+	@Insert("INSERT INTO article SET regDate = NOW(), updateDate= NOW(), title = #{title}, `body`= #{body}")
+	public Article writeArticle(String title, String body);
 
-		article.setTitle(title);	// setter 메소드
-		article.setBody(body);		// setter 메소드
-	}
-	
-	public List<Article> getArticles() {
-		return articles;
-	}
+	@Select("SELECT * FROM article ORDER BY id DESC")
+	public List<Article> getArticles();
+
+	@Select("SELECT * FROM article WHERE id = #{id}")
+	public Article getArticle(int id);
+
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(int id);
+
+	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
+	public void modifyArticle(int id, String title, String body);
+
 }
