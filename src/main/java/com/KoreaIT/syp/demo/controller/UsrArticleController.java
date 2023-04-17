eb8e494a046fe057ad60f25e813a89d25f1fb7d0
SHINYOUNG <javasyp@gmail.com>
2023-04-17 12:25:55 +0900

    ArticleService 도입 및 기능 이전

--- src/main/java/com/KoreaIT/syp/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/syp/demo/controller/UsrArticleController.java
@@ -3,116 +3,76 @@ package com.KoreaIT.syp.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.KoreaIT.syp.demo.service.ArticleService;
 import com.KoreaIT.syp.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	int lastArticleId;
-	List<Article> articles;
+	@Autowired
+	private ArticleService articleService;
 	
-	// 1. 생성자
-	public UsrArticleController() {
-		lastArticleId = 0;
-		articles = new ArrayList<>();
-		
-		makeTestData();
-	}
+//	public UsrArticleController() {
+//		articleService = new ArticleService();	// @Autowired 사용 시 필요없음.
+//	}
 	
-	// 2. 서비스 메서드
-	// 테스트 데이터
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목 " + i;
-			String body = "내용 " + i;
-			
-			writeArticle(title, body);
-		}
-	}
-	// 입력한 Id값과 article의 Id값 일치하는지?
-	private Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {	// getter 메소드
-				return article;
-			}
-		}
-		return null;
-	}
-	// 삭제
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-	}
-	// 수정
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);	// setter 메소드
-		article.setBody(body);		// setter 메소드
-	}
-	// 작성
-	public Article writeArticle(String title, String body) {
-		int id = lastArticleId + 1;
-
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		lastArticleId++;
-		
-		return article;
-	}
-	
-	// 3. 액션 메서드
+	// 액션 메서드
 	// 수정
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public Object doModify(int id, String title, String body) {		// Object
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다";
 		}
 
-		modifyArticle(id, title, body);
+		articleService.modifyArticle(id, title, body);
 
 		return article;
 	}
+	
 	// 삭제
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다.";
 		}
 
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 
 		return id + "번 글을 삭제했습니다.";
 	}
+	
 	// 작성
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		
-		Article article = writeArticle(title, body);
+		Article article = articleService.writeArticle(title, body);
 		
 		return article;
 	}
+	
 	// 목록
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		
-		return articles;
+		return articleService.articles();		// articles -> articles() 메소드 생성
 	}
+	
 	// 상세보기
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
 	public Object getArticleAction(int id) {		// Object
 
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다";
--- src/main/java/com/KoreaIT/syp/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/syp/demo/controller/UsrHomeController.java
@@ -1,10 +1,5 @@
 package com.KoreaIT.syp.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,60 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UsrHomeController {
 	
-	@RequestMapping("/usr/home/getInt")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-	
-	@RequestMapping("/usr/home/getString")
-	@ResponseBody
-	public String getString() {
-		return "안녕";
-	}
-	
-	@RequestMapping("/usr/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-	
-	@RequestMapping("/usr/home/getChar")
-	@ResponseBody
-	public char getChar() {
-		return 'a';
-	}
-	
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수 나이", 22);
-		map.put("영희 나이", 20);
+	public String showMain() {
 		
-		return map;		// {}
+		return "MAIN HI";
 	}
-	
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	public List<Object> getList() {
-		List<Object> list = new ArrayList<>();
-		list.add("철수 나이");
-		list.add(22);
-		
-		return list;	// []
-	}
-	
 }
