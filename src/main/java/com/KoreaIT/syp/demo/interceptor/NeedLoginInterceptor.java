52543e39d84315b2d61ac3bb90083a56d35c7752
SHINYOUNG <javasyp@gmail.com>
2023-04-21 14:28:34 +0900

    NeedLoginInterceptor 구현 및 MyWebMvcConfigurer 등록

--- src/main/java/com/KoreaIT/syp/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/KoreaIT/syp/demo/interceptor/NeedLoginInterceptor.java
@@ -9,12 +9,12 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import com.KoreaIT.syp.demo.vo.Rq;
 
 @Component
-public class BeforeActionInterceptor implements HandlerInterceptor {
+public class NeedLoginInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		Rq rq = new Rq(req);
-		req.setAttribute("rq", rq);
+		
+		System.out.println("로그인 후 이용해 주세요.");
 		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
